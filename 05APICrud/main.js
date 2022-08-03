e28dc0258d0403123debb99f1f72ff3f762512b4
Homero Hurtado <homerohg28@gmail.com>
2022-08-03 16:03:21 -0500

    feat: peticiones

---
+++ 05APICrud/main.js
@@ -0,0 +1,9 @@
+//* NUESTRO JS PRINCIPAL
+
+const goodreadsCrud = require ('./crudAuthors.js')
+
+//* LISTA DE TODOS LOS AUTORES
+//    goodreadsCrud.listAuthors()
+
+//* LISTA UN AUTOR POR SU ID
+goodreadsCrud.getAuthor(14614)
